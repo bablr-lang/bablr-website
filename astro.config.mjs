ea47c3ec20cc92efc7f6fb572be65f4b1720b815
Stirling Hostetter <stirlhoss@proton.me>
2024-08-13 11:10:43 -0500

    add qwik to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import qwikdev from "@qwikdev/astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: []
+  integrations: [qwikdev()]
 });
