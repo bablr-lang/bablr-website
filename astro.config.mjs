a154ef94f2f0ea002c2f76530fc89c174ec589b1
Stirling Hostetter <stirlhoss@proton.me>
2024-08-30 19:03:25 -0500

    setup for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
 
-import qwikdev from "@qwikdev/astro";
+import node from '@astrojs/node';
+
+// import qwikdev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev()]
+  integrations: [],
+  server: { port: 8080 },
+  site: 'https://bablr-website.fly.dev',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
