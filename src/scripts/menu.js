d6229f40fdf727bee9eed4306a11cc45e0d228b7
Stirling Hostetter <stirlhoss@proton.me>
2024-09-26 10:56:22 -0500

    progress

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,8 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.nav-links').classList.toggle('expanded');
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.querySelector(".nav-links").classList.toggle("expanded");
+
+  let seperators = document.querySelectorAll(".nav-seperator");
+  for (let seperator of seperators) {
+    seperator.classList.toggle("nav-seperator-expanded");
+  }
 });
