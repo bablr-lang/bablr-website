5b3656905b95f068a35243f047836dd829880841
Stirling Hostetter <stirlhoss@proton.me>
2025-01-02 16:16:31 -0600

    add keymap and alter layout

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -10,8 +10,9 @@ import {
   createCodeMirror,
   createEditorControlledValue,
 } from "solid-codemirror";
-import { lineNumbers } from "@codemirror/view";
+import { lineNumbers, keymap } from "@codemirror/view";
 import { javascript } from "@codemirror/lang-javascript";
+import { defaultKeymap } from "@codemirror/commands";
 import { solarizedLight } from "thememirror";
 import * as helpers from "@bablr/helpers";
 
@@ -47,6 +48,7 @@ export default function App() {
   createExtension(solarizedLight);
   createExtension(lineNumbers);
   createExtension(javascript);
+  createExtension(keymap.of(defaultKeymap));
 
   let enhancers = {};
 
@@ -64,104 +66,120 @@ export default function App() {
     <>
       <div id="playground" style={{ display: "flex", "flex-flow": "row" }}>
         <div
-          id="input"
-          style={{
-            display: "flex",
-            "flex-flow": "row",
-            "align-items": "center",
-          }}
+          id="playground-left"
+          style={{ display: "flex", "flex-flow": "column", width: "50%" }}
         >
           <div
-            id="input-form"
-            style={{ display: "flex", "flex-flow": "column" }}
-          >
-            <label for="#experiment-input" style={{ "text-align": "center" }}>
-              Input
-            </label>
-            <div
-              id="matcher-tag-input"
-              style={{ display: "inline-flex", gap: "1rem", padding: "10px" }}
-            >
-              <label for="#matcher-tag">Matcher: </label>
-              <select
-                id="matcher-tag"
-                onInput={(e) => {
-                  setMatcherTag(e.currentTarget.value);
-                }}
-              >
-                <For
-                  each={[
-                    ...new Set(
-                      [...productions()]
-                        .filter(
-                          ({ 0: key }) => key !== Symbol.for("@bablr/fragment"),
-                        )
-                        .map(({ 0: key }) => {
-                          let string = key;
-                          if (typeof string === "symbol") {
-                            string = `[${printType(string)}]`;
-                          }
-                          return string;
-                        }),
-                    ),
-                  ].sort()}
-                >
-                  {(key) => {
-                    return (
-                      <option selected={key === matcherTag()} value={key}>
-                        {key}
-                      </option>
-                    );
-                  }}
-                </For>
-              </select>
-            </div>
-            <textarea
-              id="experiment-input"
-              value={input()}
-              onInput={(e) => setInput(e.currentTarget.value)}
-            >
-              {input()}
-            </textarea>
-          </div>
-          <button
-            id="form-eval"
-            onClick={(e) => {
-              e.preventDefault();
-              console.log("submitting");
-              setTags(
-                streamParse(
-                  ctx(),
-                  matcher(),
-                  input(),
-                  {},
-                  { enhancers, emitEffects: true },
-                ),
-              );
+            id="input"
+            style={{
+              display: "flex",
+              "flex-flow": "row",
+              "align-items": "center",
+              height: "50%",
             }}
           >
-            eval
-          </button>
+            <div
+              id="input-form"
+              style={{
+                display: "flex",
+                "flex-flow": "column",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <label for="#experiment-input" style={{ "text-align": "center" }}>
+                Input
+              </label>
+              <div
+                id="matcher-tag-input"
+                style={{ display: "inline-flex", gap: "1rem", padding: "10px" }}
+              >
+                <label for="#matcher-tag">Matcher: </label>
+                <select
+                  id="matcher-tag"
+                  onInput={(e) => {
+                    setMatcherTag(e.currentTarget.value);
+                  }}
+                >
+                  <For
+                    each={[
+                      ...new Set(
+                        [...productions()]
+                          .filter(
+                            ({ 0: key }) =>
+                              key !== Symbol.for("@bablr/fragment"),
+                          )
+                          .map(({ 0: key }) => {
+                            let string = key;
+                            if (typeof string === "symbol") {
+                              string = `[${printType(string)}]`;
+                            }
+                            return string;
+                          }),
+                      ),
+                    ].sort()}
+                  >
+                    {(key) => {
+                      return (
+                        <option selected={key === matcherTag()} value={key}>
+                          {key}
+                        </option>
+                      );
+                    }}
+                  </For>
+                </select>
+              </div>
+              <textarea
+                id="experiment-input"
+                value={input()}
+                onInput={(e) => setInput(e.currentTarget.value)}
+              >
+                {input()}
+              </textarea>
+            </div>
+            <button
+              id="form-eval"
+              onClick={(e) => {
+                e.preventDefault();
+                console.log("submitting");
+                setTags(
+                  streamParse(
+                    ctx(),
+                    matcher(),
+                    input(),
+                    {},
+                    { enhancers, emitEffects: true },
+                  ),
+                );
+              }}
+            >
+              eval
+            </button>
+          </div>
+          <div
+            id="output"
+            style={{ display: "flex", "flex-flow": "column", height: "50%" }}
+          >
+            <label for="#experiment-output" style={{ "text-align": "center" }}>
+              Output
+            </label>
+            <textarea id="experiment-output">
+              {tags() != null
+                ? printPrettyCSTML(
+                    evaluateIO(() => tags()),
+                    { ctx: ctx() },
+                  )
+                : null}
+            </textarea>
+          </div>
         </div>
-        <div id="output" style={{ display: "flex", "flex-flow": "column" }}>
-          <label for="#experiment-output" style={{ "text-align": "center" }}>
-            Output
-          </label>
-          <textarea id="experiment-output">
-            {tags() != null
-              ? printPrettyCSTML(
-                  evaluateIO(() => tags()),
-                  { ctx: ctx() },
-                )
-              : null}
-          </textarea>
-        </div>
-
-        <div id="grammar" style={{ display: "flex", "flex-flow": "column" }}>
-          <label for="#experiment-grammar" style={{ "text-align": "center" }}>
-            Grammar
-          </label>
-          <div id="experiment-grammar" ref={ref}></div>
+        <div id="playground-right" style={{ width: "50%" }}>
+          <div id="grammar" style={{ display: "flex", "flex-flow": "column" }}>
+            <label for="#experiment-grammar" style={{ "text-align": "center" }}>
+              Grammar
+            </label>
+            <div id="experiment-grammar" ref={ref}></div>
+          </div>
         </div>
       </div>
     </>
