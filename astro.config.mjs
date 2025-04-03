6deec7c3d94615c7822b06970fe51ee85b1e95f0
Stirling Hostetter <stirlhoss@proton.me>
2025-04-03 13:11:00 -0500

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,20 +6,19 @@ import starlight from "@astrojs/starlight";
 
 import solidJs from "@astrojs/solid-js";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/experiments/playground": "/playground",
   },
-  integrations: [
-    starlight({
-      title: "DOCS",
-      logo: { src: "./src/images/BABLRTransparent.png" },
-      prerender: false,
-      favicon: "/favicon.ico",
-    }),
-    solidJs(),
-  ],
+  integrations: [starlight({
+    title: "DOCS",
+    logo: { src: "./src/images/BABLRTransparent.png" },
+    prerender: false,
+    favicon: "/favicon.ico",
+  }), solidJs(), mdx()],
   server: { port: 8080 },
   site: "https://bablr.org",
   output: "server",
