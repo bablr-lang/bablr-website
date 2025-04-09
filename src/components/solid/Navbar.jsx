42225478231c2387f4fea796af130e430775836f
Stirling Hostetter <stirlhoss@proton.me>
2025-04-09 13:50:45 -0500

    make navbar respect max-width

--- src/components/solid/Navbar.jsx
+++ src/components/solid/Navbar.jsx
@@ -12,11 +12,11 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = createSignal(false);
 
   return (
-    <header class="flex items-center justify-between p-6 relative">
+    <header class="max-w-6xl m-auto flex items-center justify-between p-6 relative">
       {/* Logo */}
       <div class="flex items-center space-x-2 flex-1">
         <a href="/" class="block">
-          <img src="/bablr_logo.png" alt="Bablr Logo" class="h-16 w-16"/>
+          <img src="/bablr_logo.png" alt="Bablr Logo" class="h-16 w-16" />
         </a>
       </div>
 
@@ -25,28 +25,26 @@ const Navbar = () => {
         class="relative w-8 h-8 lg:hidden focus:outline-none z-50"
         aria-label="Toggle menu"
         onClick={() => {
-            setIsOpen(!isOpen())}}
+          setIsOpen(!isOpen());
+        }}
       >
         <span
           classList={{
-            "block absolute h-0.5 w-6 bg-current transition-all duration-300 ease-in-out":
-              true,
+            "block absolute h-0.5 w-6 bg-current transition-all duration-300 ease-in-out": true,
             "rotate-45": isOpen(),
             "-translate-y-2": !isOpen(),
           }}
         />
         <span
           classList={{
-            "block absolute h-0.5 w-6 bg-current transition-all duration-300 ease-in-out":
-              true,
+            "block absolute h-0.5 w-6 bg-current transition-all duration-300 ease-in-out": true,
             "opacity-0": isOpen(),
             "opacity-100": !isOpen(),
           }}
         />
         <span
           classList={{
-            "block absolute h-0.5 w-6 bg-current transition-all duration-300 ease-in-out":
-              true,
+            "block absolute h-0.5 w-6 bg-current transition-all duration-300 ease-in-out": true,
             "-rotate-45": isOpen(),
             "translate-y-2": !isOpen(),
           }}
@@ -56,8 +54,7 @@ const Navbar = () => {
       {/* Mobile Menu */}
       <nav
         classList={{
-          "fixed inset-0 bg-orange-100 flex flex-col items-center justify-center z-10 transition-transform duration-300 ease-in-out lg:hidden":
-            true,
+          "fixed inset-0 bg-orange-100 flex flex-col items-center justify-center z-10 transition-transform duration-300 ease-in-out lg:hidden": true,
           "translate-x-0": isOpen(),
           "translate-x-full": !isOpen(),
         }}
