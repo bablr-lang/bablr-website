0ea2a3a0b4206638d85032a2abd698740d087b25
Conrad Buck <conartist6@gmail.com>
2025-08-13 09:42:35 -0600

    fixes

--- src/components/solid/Highlighter.js
+++ src/components/solid/Highlighter.js
@@ -1,7 +1,14 @@
-import { highlightAll } from "@bablr/highlight";
+import { highlightAll } from "bedazzlr";
+import * as cstml from "@bablr/language-en-cstml";
+import * as esnext from "@bablr/language-en-esnext";
+
+let languages = new Map([
+  [cstml.canonicalURL, cstml],
+  [esnext.canonicalURL, esnext],
+]);
 
 let Highlighter = () => {
-  highlightAll();
+  highlightAll(languages);
 };
 
 export default Highlighter;
