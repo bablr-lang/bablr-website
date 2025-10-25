77662876942814af970d05147768be41079c165c
Conrad Buck <conartist6@gmail.com>
2025-10-25 14:47:43 -0600

    flags.intrinsic

--- src/components/solid/Highlighter.js
+++ src/components/solid/Highlighter.js
@@ -1,10 +1,12 @@
 import { highlightAll } from "bedazzlr";
 import * as cstml from "@bablr/language-en-cstml";
 import * as esnext from "@bablr/language-en-esnext";
+import * as json from "@bablr/language-en-json";
 
 let languages = new Map([
   [cstml.canonicalURL, cstml],
   [esnext.canonicalURL, esnext],
+  [json.canonicalURL, json],
 ]);
 
 let Highlighter = () => {
