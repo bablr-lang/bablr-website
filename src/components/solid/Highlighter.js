89b5c956dcad0212e3eac2f8aed2877d079ef069
Conrad Buck <conartist6@gmail.com>
2026-03-22 13:51:16 -0600

    pre-release fixes

--- src/components/solid/Highlighter.js
+++ src/components/solid/Highlighter.js
@@ -3,12 +3,15 @@ import { highlightAll } from "bedazzlr";
 import cstml from "@bablr/language-en-cstml";
 import esnext from "@bablr/language-en-esnext";
 import json from "@bablr/language-en-json";
+import * as BMap from "@bablr/agast-helpers/b-map";
 
-let languages = new Map([
-  [cstml.canonicalURL, cstml],
-  [esnext.canonicalURL, esnext],
-  [json.canonicalURL, json],
-]);
+let { entry } = BMap;
+
+let languages = BMap.from(
+  entry(cstml.canonicalURL, cstml),
+  entry(esnext.canonicalURL, esnext),
+  entry(json.canonicalURL, json),
+);
 
 let Highlighter = () => {
   highlightAll(languages);
